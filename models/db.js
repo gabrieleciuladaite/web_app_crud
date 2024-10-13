316c0c86aab06130e24b947f7a7f134fb93722a0
gabrieleciuladaite <gabriele.ciuladaite@gmail.com>
2024-10-13 17:37:16 +0300

    db

--- models/db.js
+++ models/db.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+
+const {WEB_APP_NOTES_MONGODB_HOST,WEB_APP_NOTES_MONGODB_DATABASE} = process.env;
+console.log(WEB_APP_NOTES_MONGODB_HOST)
+console.log(WEB_APP_NOTES_MONGODB_DATABASE)
+const MONGODB_CONNECTION = `mongodb://${WEB_APP_NOTES_MONGODB_HOST}/${WEB_APP_NOTES_MONGODB_DATABASE}`;
+
+
+mongoose.connect(MONGODB_CONNECTION, { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
