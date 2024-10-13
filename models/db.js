b372ca21d810e70fbac707a3a788207f5596a6a2
gabrieleciuladaite <gabriele.ciuladaite@gmail.com>
2024-10-13 18:18:58 +0300

    .

--- models/db.js
+++ models/db.js
@@ -7,7 +7,7 @@ console.log(WEB_APP_NOTES_MONGODB_DATABASE)
 const MONGODB_CONNECTION = `mongodb://${WEB_APP_NOTES_MONGODB_HOST}/${WEB_APP_NOTES_MONGODB_DATABASE}`;
     
 
-mongoose.connect('mongodb+srv://gabrieleciuladaite:gabrieleciuladaite@cluster0.cw2h9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/STUDENTS', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb://gabrieleciuladaitee:UAMcSE4ekQmRuvvr@cluster0.cw2h9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/STUDENTS', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
