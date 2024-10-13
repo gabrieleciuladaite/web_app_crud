758ba21c16d03ad4bfad0d11d46f583188e49fdf
gabrieleciuladaite <gabriele.ciuladaite@gmail.com>
2024-10-13 17:51:24 +0300

    .

--- models/db.js
+++ models/db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 
-const {WEB_APP_NOTES_MONGODB_HOST,WEB_APP_NOTES_MONGODB_DATABASE} = process.env;
-console.log(WEB_APP_NOTES_MONGODB_HOST)
-console.log(WEB_APP_NOTES_MONGODB_DATABASE)
-const MONGODB_CONNECTION = `mongodb://${WEB_APP_NOTES_MONGODB_HOST}/${WEB_APP_NOTES_MONGODB_DATABASE}`;
+// const {WEB_APP_NOTES_MONGODB_HOST,WEB_APP_NOTES_MONGODB_DATABASE} = process.env;
+// console.log(WEB_APP_NOTES_MONGODB_HOST)
+// console.log(WEB_APP_NOTES_MONGODB_DATABASE)
+// const MONGODB_CONNECTION = `mongodb://${WEB_APP_NOTES_MONGODB_HOST}/${WEB_APP_NOTES_MONGODB_DATABASE}`;
+    
 
-
-mongoose.connect(MONGODB_CONNECTION, { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb://gabrieleciuladaite:rvfYClmbe3yRVSGI@cluster0.cw2h9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/STUDENTS', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
